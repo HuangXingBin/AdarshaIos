ebe7e64d7ed48f24393321b795d8a9c4d50b7f57
HuangXingBin <356578436@qq.com>
2016-09-22 00:31:15 +0800

    最后一个uti找不到的bug fix

--- src/constants/AppConstants.js
+++ src/constants/AppConstants.js
@@ -1,3 +1,3 @@
-// export const DB_NAME = 'jiangkangyur';
-export const DB_NAME = 'sample_new';
+export const DB_NAME = 'jiangkangyur';
+// export const DB_NAME = 'sample_new';
 export const MENU_WIDTH = 211;
